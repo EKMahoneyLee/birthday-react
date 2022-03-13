2fe172edea609200f1a52c5da8952645ada0fc14
EK <seosi0825@hotmail.com>
2022-03-13 17:55:49 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Bbangsoon Lee',
+    age: 7.3,
+    image:['../images/B.jpg'],
+  },
+  {
+    id: 2,
+    name: 'Ek Lee',
+    age: 17,
+    image:['../images/EK.jpg'],
+  },
+  {
+    id: 3,
+    name: 'Aegi Lee',
+    age: 8,
+    image:['../images/A.jpg'],
+  },
+  {
+    id: 4,
+    name: 'Sean Mahoney',
+    age: 34,
+    image:['../images/S.jpg'],
+  },
+  {
+    id: 5,
+    name: 'Summer Lee',
+    age: 14,
+    image:['../images/Summer.jpg'],
+  },
+];
